5567dcbed0f7f412ddc6c1870cbbebec78b5112f
Torben Jost <torbenjost@proton.me>
2025-06-01 12:30:21 +0200

    add models for exercise, workout and workoutlog

---
+++ backend/src/main/kotlin/workouttracker/backend/model/WorkoutLog.kt
@@ -0,0 +1,17 @@
+package workouttracker.backend.model
+
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
+import workouttracker.backend.model.enums.TrainingGoal
+import java.time.LocalDateTime
+
+@Document(collection = "workoutLog")
+data class WorkoutLog(
+    @Id val id: String,
+    @CreatedDate
+    val createdAt: LocalDateTime? = null,
+    val trainingGoal: TrainingGoal,
+    val workout: Workout,
+    val comment: String = ""
+)
