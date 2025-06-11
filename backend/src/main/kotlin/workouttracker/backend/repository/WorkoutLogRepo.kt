58c74e6f10c7d04e06b81064c2ded68703dabf23
Torben Jost <torbenjost@proton.me>
2025-06-11 18:22:11 +0200

    create more detailed search workout log functionalities

--- backend/src/main/kotlin/workouttracker/backend/repository/WorkoutLogRepo.kt
+++ backend/src/main/kotlin/workouttracker/backend/repository/WorkoutLogRepo.kt
@@ -3,8 +3,11 @@ package workouttracker.backend.repository
 import org.springframework.data.mongodb.repository.MongoRepository
 import org.springframework.stereotype.Repository
 import workouttracker.backend.model.WorkoutLog
+import workouttracker.backend.model.enums.Mon
 import workouttracker.backend.model.enums.TrainingGoal
 import java.time.LocalDateTime
+import java.time.Month
+import java.util.*
 
 @Repository
 interface WorkoutLogRepo : MongoRepository<WorkoutLog, String> {
@@ -13,4 +16,8 @@ interface WorkoutLogRepo : MongoRepository<WorkoutLog, String> {
     fun findByTrainingGoal(trainingGoal: TrainingGoal): List<WorkoutLog>
     fun findByWorkoutId(workoutId: String): List<WorkoutLog>
     fun deleteWorkoutLogById(workoutId: String);
+    fun findTopByWorkoutIdOrderByCreatedAtDesc(workoutId: String): Optional<WorkoutLog>;
+    fun findWorkoutLogsByMon(mon: Mon): List<WorkoutLog>
+    fun findFiveLatestWorkoutLogs(date: LocalDateTime): List<WorkoutLog>
+    fun findByCreatedAtBetween(startDate: LocalDateTime, endDate: LocalDateTime): List<WorkoutLog>
 }
