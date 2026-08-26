647b52efa4d3401e4c58b62a8ec7eb2252036321
Torben Jost <165148729+turbo-91@users.noreply.github.com>
2026-08-26 21:11:03 +0200

    feat: warmUps
    create WarmUpSet Model and wire it to db

---
+++ backend/src/main/java/com/tjost/workouttracker/model/WarmUpSet.java
@@ -0,0 +1,39 @@
+package com.tjost.workouttracker.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "warm_up_sets")
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class WarmUpSet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "warm_up_set_id")
+    @Setter(AccessLevel.NONE)
+    private Long warmUpSetId;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "plan_item_id", nullable = false)
+    private WorkoutPlanItem workoutPlanItem;
+
+    @Column(name = "set_number",  nullable = false)
+    private short setNumber;
+
+    @Column(name = "weight_kg", nullable = false)
+    private BigDecimal weightKg;
+
+    @Column(name = "rep_min", nullable = false)
+    private short repMin;
+
+    @Column(name = "rep_max", nullable = false)
+    private short repMax;
+
+    @Column(name = "rest_seconds", nullable = false)
+    private short restSeconds;
+}
